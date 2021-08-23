5b06957659b666c40448ce760bcd06c3e6cdd391
Jaken Herman <JakenHerman7@Gmail.com>
2021-08-22 23:12:37 -0500

    Set firebase json credentials in env

--- __init__.py
+++ __init__.py
@@ -22,7 +22,19 @@ reddit = praw.Reddit(
   )
 
 # Use the application default credentials
-cred = credentials.Certificate(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
+cred = credentials.Certificate({
+  "type": "service_account",
+  "project_id": "lilstitiousbot",
+  "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
+  "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+  "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
+  "client_id": os.getenv('FIREBASE_CLIENT_ID'),
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-33av6%40lilstitiousbot.iam.gserviceaccount.com"
+})
+
 firebase_admin.initialize_app(cred, {
   'projectId': 'lilstitiousbot',
 })
