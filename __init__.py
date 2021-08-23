d7af4a048b250bd685f8853e2b0e15fb1ab729ef
Jaken Herman <JakenHerman7@Gmail.com>
2021-08-23 00:24:41 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -26,7 +26,7 @@ cred = credentials.Certificate({
   "type": "service_account",
   "project_id": "lilstitiousbot",
   "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
-  "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+  "private_key": os.getenv('FIREBASE_PRIVATE_KEY'),
   "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
   "client_id": os.getenv('FIREBASE_CLIENT_ID'),
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
