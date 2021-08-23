2d64b82c832fc265f94c1607957911650b645984
Jaken Herman <JakenHerman7@Gmail.com>
2021-08-22 22:20:40 -0500

    utilizing firebase instead of text file now

--- __init__.py
+++ __init__.py
@@ -1,9 +1,19 @@
 import os
 import praw
-import time
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
-def login():
-  return praw.Reddit(
+def read_comments(reddit, comments_already_responded_to, db):
+  for comment in reddit.subreddit('dundermifflin').comments(limit=1000):
+    if "superstitious" in comment.body.lower() and comment.id not in comments_already_responded_to and comment.author != reddit.user.me():
+      comment.reply("I'm not superstitious, but I am a little stitious.")
+      doc_ref = db.collection(u'comment_ids').document(comment.id)
+      doc_ref.set({
+          u'body': comment.body
+      })
+  
+reddit = praw.Reddit(
     username = os.getenv('REDDIT_USERNAME'),
 		password = os.getenv('REDDIT_PASSWORD'),
 		client_id = os.getenv('API_CLIENT'),
@@ -11,23 +21,15 @@ def login():
 		user_agent = "Little Stitious Bot"
   )
 
-def read_comments(reddit, comments_already_responded_to):
-  for comment in reddit.subreddit('dundermifflin').comments(limit=1000):
-    if "superstitious" in comment.body.lower() and comment.id not in comments_already_responded_to and comment.author != reddit.user.me():
-      comment.reply("I'm not superstitious, but I am a little stitious.")
-      comments_already_responded_to.append(comment.id)
-      with open("comment_ids.txt", "a") as ids:
-        ids.write(comment.id + "\n")
-  time.sleep(120)
+# Use the application default credentials
+cred = credentials.Certificate("adminsdk.json")
+firebase_admin.initialize_app(cred, {
+  'projectId': 'lilstitiousbot',
+})
 
-def get_saved_comments():
-	if os.path.isfile("comment_ids.txt"):
-		with open("comment_ids.txt", "r") as f:
-			return f.read().split("\n")
-	return []
+db = firestore.client()
 
-        
-reddit = login()
+saved_comments = db.collection(u'comment_ids')
+docs = saved_comments.stream()
 
-while True:
-  read_comments(reddit, get_saved_comments())
+read_comments(reddit, [doc.id for doc in docs], db)
