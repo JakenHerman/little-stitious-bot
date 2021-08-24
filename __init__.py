515db03b39001148976372c64dc8d3c426d76ffe
Jaken Herman <JakenHerman7@Gmail.com>
2021-08-23 23:42:57 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,10 +14,10 @@ def read_comments(reddit, comments_already_responded_to, db):
       })
   
 reddit = praw.Reddit(
-    username = os.environ.get('REDDIT_USERNAME'),
-		password = os.environ.get('REDDIT_PASSWORD'),
-		client_id = os.environ.get('API_CLIENT'),
-		client_secret = os.environ.get('API_SECRET'),
+    username = os.getenv('REDDIT_USERNAME'),
+		password = os.getenv('REDDIT_PASSWORD'),
+		client_id = os.getenv('API_CLIENT'),
+		client_secret = os.getenv('API_SECRET'),
 		user_agent = "Little Stitious Bot"
   )
 
@@ -25,10 +25,10 @@ reddit = praw.Reddit(
 cred = credentials.Certificate({
   "type": "service_account",
   "project_id": "lilstitiousbot",
-  "private_key_id": os.environ.get('FIREBASE_PRIVATE_KEY_ID'),
-  "private_key": os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
-  "client_email": os.environ.get('FIREBASE_CLIENT_EMAIL'),
-  "client_id": os.environ.get('FIREBASE_CLIENT_ID'),
+  "private_key_id": os.getenv('FIREBASE_PRIVATE_KEY_ID'),
+  "private_key": os.getenv('FIREBASE_PRIVATE_KEY').replace('\\n', '\n'),
+  "client_email": os.getenv('FIREBASE_CLIENT_EMAIL'),
+  "client_id": os.getenv('FIREBASE_CLIENT_ID'),
   "auth_uri": "https://accounts.google.com/o/oauth2/auth",
   "token_uri": "https://oauth2.googleapis.com/token",
   "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
