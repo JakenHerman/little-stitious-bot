fa13404e4c2b27f1700437bb510d9ffdbeaf2301
Jaken Herman <JakenHerman7@Gmail.com>
2021-08-30 19:28:14 -0500

    ditched firebase, went to supabase

--- __init__.py
+++ __init__.py
@@ -1,19 +1,9 @@
 import os
 import praw
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from supabase_py import create_client, Client
 
-def read_comments(reddit, comments_already_responded_to, db):
-  for comment in reddit.subreddit('dundermifflin').comments(limit=1000):
-    if "superstitious" in comment.body.lower() and comment.id not in comments_already_responded_to and comment.author != reddit.user.me():
-      comment.reply("I'm not superstitious, but I am a little stitious.")
-      doc_ref = db.collection(u'comment_ids').document(comment.id)
-      doc_ref.set({
-          u'body': comment.body
-      })
-  
-reddit = praw.Reddit(
+def read_comments():
+  reddit = praw.Reddit(
     username = os.environ.get('REDDIT_USERNAME'),
 		password = os.environ.get('REDDIT_PASSWORD'),
 		client_id = os.environ.get('API_CLIENT'),
@@ -21,27 +11,15 @@ reddit = praw.Reddit(
 		user_agent = "Little Stitious Bot"
   )
 
-# Use the application default credentials
-cred = credentials.Certificate({
-  "type": "service_account",
-  "project_id": "lilstitiousbot",
-  "private_key_id": os.environ.get('FIREBASE_PRIVATE_KEY_ID'),
-  "private_key": str("-----BEGIN PRIVATE KEY-----\n" + str(os.environ.get('FIREBASE_PRIVATE_KEY').replace('\\n', '\n')) + "\n-----END PRIVATE KEY-----\n"),
-  "client_email": os.environ.get('FIREBASE_CLIENT_EMAIL'),
-  "client_id": os.environ.get('FIREBASE_CLIENT_ID'),
-  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-  "token_uri": "https://oauth2.googleapis.com/token",
-  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-33av6%40lilstitiousbot.iam.gserviceaccount.com"
-})
+  url: str = os.environ.get("SUPABASE_URL")
+  key: str = os.environ.get("SUPABASE_KEY")
+  supabase: Client = create_client(url, key)
+  comments = supabase.table("comment_ids").select("*").execute()
+  comments_already_responded_to = [comment.id for comment in comments.get("data", [])]
 
-firebase_admin.initialize_app(cred, {
-  'projectId': 'lilstitiousbot',
-})
+  for comment in reddit.subreddit('dundermifflin').comments(limit=100000):
+    if "superstitious" in comment.body.lower() and comment.id not in comments_already_responded_to and comment.author != reddit.user.me():
+      comment.reply("I'm not superstitious, but I am a little stitious.")
+      data = supabase.table("comment_ids").insert({"id":comment.id,"comment_body":comment.body}).execute()
 
-db = firestore.client()
-
-saved_comments = db.collection(u'comment_ids')
-docs = saved_comments.stream()
-
-read_comments(reddit, [doc.id for doc in docs], db)
+read_comments()
